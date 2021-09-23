a0d4e3e2e5028f5f309ecd78b808cc46612131b2
Vyacheslav <nonoemail@email.com>
2021-09-23 16:06:30 +0700

    config eslint rulse

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const helloUserName = () => {
-    const name = readlineSync.question('May I have your name?: ');
-    return `Hello, ${name}`;
+  const name = readlineSync.question('May I have your name?: ');
+  return `Hello, ${name}!`;
 };
 
 export default helloUserName;
