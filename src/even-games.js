3c2274774adaea5451c4775b9da1e7cb4415776b
Vyacheslav <nonoemail@email.com>
2021-09-24 15:47:47 +0700

    create even-game

---
+++ src/even-games.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+
+const randomNumber = () => Math.floor(Math.random() * (100 - 1) + 1);
+
+const evenGames = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    const question = randomNumber();
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('You answer: ');
+    const checkIsEven = question % 2 === 0;
+    if (answer === 'yes' && checkIsEven) {
+      console.log('Correct !');
+    } else if (answer === 'no' && !checkIsEven) {
+      console.log('Correct !');
+    } else {
+      console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.');
+      console.log('Let\'s try again, Slavok');
+      return;
+    }
+  }
+  console.log('Congratulations!');
+};
+console.log(evenGames());
+export default evenGames;
